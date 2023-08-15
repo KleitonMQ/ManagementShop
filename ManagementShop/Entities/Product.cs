829877c819b3562ea74b7f7546f30ebdfb4ad864
Kleiton MQ <kleiton.x5@gmail.com>
2023-08-14 21:02:08 -0300

    ajustes no formulario para permitir valores decimais.

--- ManagementShop/Entities/Product.cs
+++ ManagementShop/Entities/Product.cs
@@ -11,7 +11,7 @@ namespace DonaMenina.Entities
         public string ProductName { get; set; }
         public string Brand{ get; set; }
         public string Size { get; set; }
-        public int Price { get; set; }
+        public decimal Price { get; set; }
         public int Quantity { get; set; }
     }
 }
--- ManagementShop/wwwroot/js/site.js
+++ ManagementShop/wwwroot/js/site.js
@@ -8,3 +8,17 @@
 
 showDateTime();
 setInterval(showDateTime, 1000);
+
+function formatValue(input) {
+    // Remove todos os caracteres, exceto números e vírgula
+    let valor = input.value.replace(/[^\d,]/g, '');
+
+    // Remove vírgulas duplicadas
+    valor = valor.replace(/(,.*?),/g, '$1');
+
+    // Formata os centavos (últimos dois dígitos após a vírgula)
+    valor = valor.replace(/(\d+),(\d{0,2}).*/, '$1,$2');
+
+    // Atualiza o valor no campo de entrada
+    input.value = valor;
+}
